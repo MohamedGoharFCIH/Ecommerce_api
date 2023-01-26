ff3b9d7536fd10374f06627642c66a67048fcdbe
mohamed gohar <engmabdo85@gmail.com>
2023-01-26 03:17:42 +0200

    add helper

---
+++ helpers/helper.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+
+exports.objId = (id) => {
+   if ((typeof id === 'string' || id instanceof String)) {
+      if (id.length == 12 || id.length == 24)
+         return mongoose.Types.ObjectId(id)
+   }
+   else{
+      return id
+   }
+
+}
